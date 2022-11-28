8070053e98ca312ff799df9a274bbbfaa77df751
sandrafepu <sandrafepu@gmail.com>
2022-11-28 10:34:30 +0000

    Subida de proyecto card

--- src/app.js
+++ src/app.js
@@ -6,6 +6,17 @@ import "./assets/img/rigo-baby.jpg";
 import "./assets/img/4geeks.ico";
 
 window.onload = function() {
-  //write your code here
-  console.log("Hello Rigo from the console!");
+  document.querySelector(".number").innerHTML = generateRandomNumber();
+  function generateRandomNumber() {
+    let numbers = [2, 3, 4, 5, 6, 7, 8, 9, 10, "J", "Q", "K"];
+    let resultOfNumbers = Math.floor(Math.random() * numbers.length);
+    return numbers[resultOfNumbers];
+  }
+  let suit = document.querySelector(".card");
+  suit.classList.add(generateRandomSuit());
+  function generateRandomSuit() {
+    let suits = ["diamond", "heart", "club", "spade"];
+    let resultOfSuits = Math.floor(Math.random() * suits.length);
+    return suits[resultOfSuits];
+  }
 };
